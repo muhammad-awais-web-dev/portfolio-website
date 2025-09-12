e2cc106b1cb405a88e35d0b34ec8a2d16150b96d
Muhammad Awais <awaisrafique4929@gmail.com>
2025-09-12 21:40:01 +0500

    /

--- src/pages/Home/HomeCertifications.js
+++ src/pages/Home/HomeCertifications.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import CertificationsList from '../../components/CertificationsList';
 import Styles from './HomeCertifications.module.css';
+import { Link } from 'react-router-dom';
 
 function HomeCertifications() {
   return (
     <section id="certifications" className={Styles.homeCertifications}>
       <h2 className={Styles.sectionTitle}>Certifications</h2>
       <CertificationsList showFeaturedOnly={true} />
-      <a href="/certifications" className={Styles.viewAllButton}>View All Certifications <i className="fa-solid fa-arrow-right"></i></a>
+      <Link to="/certifications" className={Styles.viewAllButton}>View All Certifications <i className="fa-solid fa-arrow-right"></i></Link>
     </section>
   )
 }
